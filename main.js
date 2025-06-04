c63085a285a56d036f9f671c90d69347a57615eb
chizao33 <chizao33@gmail.com>
2025-06-04 19:01:37 +0800

    add main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: chizao33 chizao33@gmail.com
+ * @Date: 2025-06-04 18:52:56
+ * @LastEditors: chizao33 chizao33@gmail.com
+ * @LastEditTime: 2025-06-04 18:54:48
+ * @FilePath: \git-demo\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
